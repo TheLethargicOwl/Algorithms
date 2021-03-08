9f970c2d0caaa76aacd05f0a4cb27bae9b6faefe
TheLethargicOwl <43077039+TheLethargicOwl@users.noreply.github.com>
2021-03-09 03:06:54 +0530

    Create ordered_set.cpp

---
+++ ordered_set.cpp
@@ -0,0 +1,10 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
